1e8273c8eb112a67843c7a035499203a888623c8
Daniel Soto <dasg.1993@gmail.com>
2021-08-28 09:43:15 -0500

    [data] - data types

---
+++ index.js
@@ -0,0 +1,62 @@
+let t = 16; // t is a number
+let t = "Teresa" // t is a string
+let t = true // t is a boolean
+let t; // t is a undefined
+let t = null // t is a null
+let t = {} // t is a object
+let t = [] // t is a array
+
+// numbers
+
+let num1 = 93;
+let num2 = 93.00;
+let num3 = 977e8; // 977000000
+let num4 = 977e-8; // 0.000000977
+let num5 = 999999999999999999; // 999999999999999999
+let num6 = 9999999999999999999; // 1000000000000000000;
+
+let num7 = 5 / 0 // Infinity 1.797693134862315E+308
+let num8 = -5 / 0 // -Infinity; -1.797693134862315E+308
+
+let x = 20 / "fish" // NaN 
+
+// strings
+
+let singleQuote = 'I\'m a single quote';
+let doubleQuote = "I'm a double quote";
+
+// boolean
+
+let y = 500 > 10; // true
+
+let z = 4 < 2; // false
+
+5 == 5 // true
+500 = 400 // false
+
+"1" == 1 // true
+"1" === 1 // false
+let isStudent = true;
+
+// Arrays
+
+let arr = ["APPLE", "pear", "orange"];
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [false,true, {number: 1}];
+
+// Objects
+// name: value pairs
+let obj = {
+    name: "Teresa",
+    age: 23,
+    isStudent: true,
+    "name": "Pedro"
+};
+
+let name = 'Surf' + 5 + 3;
+
+// Surf53;
+
+let number8 = 5 + 3 + "Surf";
+
+// 8Surf;
